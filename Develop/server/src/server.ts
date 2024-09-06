6f6045d20cf9125efba4d023cc4f390506e25f58
Molly Howard <mollyhoward319@gmail.com>
2024-09-05 22:39:32 -0500

    Update server.ts

--- Develop/server/src/server.ts
+++ Develop/server/src/server.ts
@@ -1,25 +1,45 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import type { Request, Response } from 'express';
+import { OpenAI } from "@langchain/openai";
+import { PromptTemplate } from "@langchain/core/prompts";
+import { z } from "zod";
+import { StructuredOutputParser, OutputFixingParser } from 'langchain/output_parsers';
+
 dotenv.config();
 
-// Import the routes
-import routes from './routes/index.js';
+const port = process.env.PORT || 3001;
+const apiKey = process.env.OPENAI_API_KEY;
 
-// const termData = JSON.parse(fs.readFileSync('./data/terms.json', 'utf-8'));
+// Check if the API key is defined
+if (!apiKey) {
+  console.error('OPENAI_API_KEY is not defined. Exiting...');
+  process.exit(1);
+}
 
 const app = express();
-const PORT = process.env.PORT || 3001;
-
-// TODO: Serve static files of entire client dist folder
-app.use(express.static('../client/dist'));
-
-// TODO: Implement middleware for parsing JSON and urlencoded form data
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-// TODO: Implement middleware to connect the routes
-app.use(routes);
+const promptFunc = async (input: string) => {
+ 
+app.post('/forecast', async (req: Request, res: Response): Promise<void> => {
+  try {
+    const location: string = req.body.location;
+    if (!location) {
+      res.status(400).json({
+        error: 'Please provide a location in the request body.',
+      });
+    }
+    const result: any = await promptFunc(location);
+    res.json({ result });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error('Error:', error.message);
+    }
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
-// Start the server on the port
-app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`)
-);
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
